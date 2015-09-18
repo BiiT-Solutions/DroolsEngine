8d295955b1b357d39390edcb13c7365143b581a1
Jorge Hortelano <jhortelano@biit-solutions.com>
2015-09-18 16:56:48 +0200

    Added some extra new log lines.

--- drools-engine-core/src/main/java/com/biit/drools/global/variables/json/DroolsGlobalVariablesFromJson.java
+++ drools-engine-core/src/main/java/com/biit/drools/global/variables/json/DroolsGlobalVariablesFromJson.java
@@ -1,5 +1,6 @@
 package com.biit.drools.global.variables.json;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,6 +10,7 @@ import com.biit.drools.global.variables.DroolsVariableDataDate;
 import com.biit.drools.global.variables.DroolsVariableDataNumber;
 import com.biit.drools.global.variables.DroolsVariableDataPostalCode;
 import com.biit.drools.global.variables.DroolsVariableDataText;
+import com.biit.drools.logger.DroolsEngineLogger;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -21,12 +23,20 @@ public class DroolsGlobalVariablesFromJson {
 	public static List<DroolsGlobalVariable> fromJson(String jsonString) {
 		GsonBuilder gsonBuilder = new GsonBuilder();
 		gsonBuilder.registerTypeAdapter(DroolsGlobalVariable.class, new DroolsGlobalVariableDeserializer());
-		gsonBuilder.registerTypeAdapter(DroolsVariableData.class, new GenericVariableDataDeserializer<DroolsVariableData>());
+		gsonBuilder.registerTypeAdapter(DroolsVariableData.class,
+				new GenericVariableDataDeserializer<DroolsVariableData>());
 		gsonBuilder.registerTypeAdapter(DroolsVariableDataNumber.class, new DroolsVariableDataNumberDeserializer());
 		gsonBuilder.registerTypeAdapter(DroolsVariableDataText.class, new DroolsVariableDataTextDeserializer());
 		gsonBuilder.registerTypeAdapter(DroolsVariableDataDate.class, new DroolsVariableDataDateDeserializer());
-		gsonBuilder.registerTypeAdapter(DroolsVariableDataPostalCode.class, new DroolsVariableDataPostalCodeDeserializer());
+		gsonBuilder.registerTypeAdapter(DroolsVariableDataPostalCode.class,
+				new DroolsVariableDataPostalCodeDeserializer());
 		Gson gson = gsonBuilder.create();
-		return Arrays.asList(gson.fromJson(jsonString, DroolsGlobalVariable[].class));
+		DroolsGlobalVariable[] variables = gson.fromJson(jsonString, DroolsGlobalVariable[].class);
+		if (variables == null) {
+			DroolsEngineLogger.warning(DroolsGlobalVariablesFromJson.class.getName(),
+					"Global variables are null! Probably the file is not correct.");
+			return new ArrayList<DroolsGlobalVariable>();
+		}
+		return Arrays.asList(variables);
 	}
 }
